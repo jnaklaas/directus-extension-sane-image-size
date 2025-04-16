597511e67963e6bc63cfdcbddf01c287d5bf333f
jnaklaas <janklaas@wildlymild.com>
2025-04-16 09:17:51 +0200

    [wip] load target format from env + quality per format - needs more testing

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import { defineHook } from "@directus/extensions-sdk";
 export default defineHook(({ action }, { services, logger, env }) => {
   const { AssetsService, FilesService } = services;
   const quality = env.EXTENSIONS_SANE_IMAGE_SIZE_UPLOAD_QUALITY ?? 60;
-  const maxSize = env.EXTENSIONS_SANE_IMAGE_SIZE_MAXSIZE ?? 1920;
+  const maxSize = env.EXTENSIONS_SANE_IMAGE_SIZE_MAXSIZE ?? 2400;
+  // const format = env.EXTENSIONS_SANE_IMAGE_SIZE_FORMAT ?? "avif";
+  // const qualityPerFormat = env.EXTENSIONS_SANE_IMAGE_SIZE_UPLOAD_QUALITY_PER_FORMAT ?? null;
 
   action("files.upload", async ({ payload, key }, context) => {
     if (payload.optimized !== true) {
+      // const transformation = getTransformation(payload.type, quality, format, qualityPerFormat, maxSize);
       const transformation = getTransformation(payload.type, quality, maxSize);
+      // console.log(JSON.stringify(transformation,null, 2));
       if (transformation !== undefined) {
         const serviceOptions = { ...context, knex: context.database };
         const assets = new AssetsService(serviceOptions);
@@ -37,16 +41,20 @@ export default defineHook(({ action }, { services, logger, env }) => {
   });
 });
 
+// function getTransformation(type, quality, format, qualityPerFormat, maxSize) {
 function getTransformation(type, quality, maxSize) {
   const format = type.split("/")[1] ?? "";
   if (["jpg", "jpeg", "png", "webp", "tiff", "avif"].includes(format)) {
-    const transforms = [["withMetadata"]];
-    if (format === "jpeg" || format === "jpg") {
-      transforms.push([format, { progressive: true }]);
-    }
+    const transforms = [
+      ['avif', {quality}]
+    ];
+    // if(qualityPerFormat) {
+    //   qualityPerFormat = JSON.parse(qualityPerFormat);
+    //   if(Object.hasOwn(qualityPerFormat, format)) quality = parseInt(qualityPerFormat[format]);
+    // }
     return {
       transformationParams: {
-        format,
+        format: 'avif',
         quality,
         width: maxSize,
         height: maxSize,
